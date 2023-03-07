7bb40f5f3960a74d96a286355700deb2edf4d6da
Richard McEwan <richardmcewan@Administrators-MacBook-Pro.local>
2023-03-07 21:48:45 +0000

    cpu verks

---
+++ emulating-a-cpu-p171/target/debug/deps/emulating_a_cpu_p171-af546ea3599fef13.d
@@ -0,0 +1,5 @@
+/Users/richardmcewan/code/rust_learning/emulating-a-cpu-p171/target/debug/deps/emulating_a_cpu_p171-af546ea3599fef13.rmeta: src/main.rs
+
+/Users/richardmcewan/code/rust_learning/emulating-a-cpu-p171/target/debug/deps/emulating_a_cpu_p171-af546ea3599fef13.d: src/main.rs
+
+src/main.rs:
---
+++ emulating-a-cpu-p171/target/debug/deps/emulating_a_cpu_p171-b09d4643f511c5e2.d
@@ -0,0 +1,5 @@
+/Users/richardmcewan/code/rust_learning/emulating-a-cpu-p171/target/debug/deps/emulating_a_cpu_p171-b09d4643f511c5e2.rmeta: src/main.rs
+
+/Users/richardmcewan/code/rust_learning/emulating-a-cpu-p171/target/debug/deps/emulating_a_cpu_p171-b09d4643f511c5e2.d: src/main.rs
+
+src/main.rs:
